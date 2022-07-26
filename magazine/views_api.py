e42ddb021f9412685d63a8e36ea8cde2034094b6
Arzaev <email@arzaev.com>
2022-07-26 21:50:19 +0200

    without serializator

--- magazine/views_api.py
+++ magazine/views_api.py
@@ -1,8 +1,19 @@
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import ArticleCategory, ArticleSubCategory, Article, Tag
 from .serializers import MainSerializer
 
 
-class MainAPIView(generics.ListAPIView):
-    queryset = Article.objects.all()
-    serializer_class = MainSerializer
+class MainAPIView(APIView):
+    def get(self, request):
+        lst = Article.objects.all().values()
+        return Response({'articles': lst})
+
+    def post(self, request):
+        return Response({'post': 'yeah'})
+
+
+# class MainAPIView(generics.ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = MainSerializer
